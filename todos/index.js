a2ce2b0afaa9985bbef1e5c10fe35f166861d96e
Han.sungyoup01 <Han.sungyoup01@eland.co.kr>
2020-04-29 17:29:58 +0900

    the first commit

---
+++ todos/index.js
@@ -0,0 +1,26 @@
+const electron = require('electron');
+const {app, BrowserWindow, Menu } = electron;
+
+
+let mainWindow;
+app.on('ready', ()=>{
+    mainWindow = new BrowserWindow({});
+    mainWindow.loadURL(`file://${__dirname}/main.html`);
+
+    const mainMenu = Menu.buildFromTemplate(menuTemplate);
+
+    Menu.setApplicationMenu(mainMenu);
+});
+
+const menuTemplate = [   
+    {
+        label: 'File',
+        submenu:[
+            { label: 'Add Todo' },
+        ]
+    }
+];
+
+if(process.platform==='darwin'){
+    menuTemplate.unshift({});
+}
